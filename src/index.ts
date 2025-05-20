dccd05f06075ba2a69de6f40f784b6019b52851e
Amjad Salah <amjad.alzubeir@gmail.com>
2025-05-20 08:14:00 +0300

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
